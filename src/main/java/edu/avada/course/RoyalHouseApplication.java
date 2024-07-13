cab6711695931742d9af9f18c803b439a2cbe317
Andriy Sichniy <sichniy.andriy@gmail.com>
2024-07-13 17:23:33 +0300

    add main

---
+++ src/main/java/edu/avada/course/RoyalHouseApplication.java
@@ -0,0 +1,12 @@
+package edu.avada.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoyalHouseApplication {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(RoyalHouseApplication.class, args);
+	}
+}
