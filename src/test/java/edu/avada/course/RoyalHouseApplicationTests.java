bfe22a380a774a6f548156b4cf84d818dc237bc9
Andriy Sichniy <sichniy.andriy@gmail.com>
2024-07-13 17:24:28 +0300

    add tests

---
+++ src/test/java/edu/avada/course/RoyalHouseApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.avada.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoyalHouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
