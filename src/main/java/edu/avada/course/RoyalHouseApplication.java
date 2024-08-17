1fd08787cfee3b48920bc50f543af38ea931ff3a
Andriy Sichniy <sichniy.andriy@gmail.com>
2024-08-17 12:30:37 +0300

    repairing

--- src/main/java/edu/avada/course/RoyalHouseApplication.java
+++ src/main/java/edu/avada/course/RoyalHouseApplication.java
@@ -8,5 +8,9 @@ public class RoyalHouseApplication {
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(RoyalHouseApplication.class, args);
+		for (int i = 0; i < 3; ++i) {
+			System.out.print("Hello ");
+		}
+		System.out.println("Welcome to Royal House!");
 	}
 }
--- src/main/java/edu/avada/course/controller/AboutController.java
+++ src/main/java/edu/avada/course/controller/AboutController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/about_company")
+@RequestMapping("/about-company")
 public class AboutController {
     @GetMapping
     public String aboutCompany(Model model) throws IOException {
